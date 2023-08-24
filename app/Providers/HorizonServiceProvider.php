f2dfcee3aeb555545f6f2d1d99c3cd83941582e0
Nikolaj Buus Kofoed <niko@esmiley.dk>
2023-08-24 14:48:44 +0200

    Remove registration

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Horizon\Horizon;
 use Laravel\Horizon\HorizonApplicationServiceProvider;
@@ -29,7 +30,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
      */
     protected function gate(): void
     {
-        Gate::define('viewHorizon', function ($user) {
+        Gate::define('viewHorizon', function (User $user) {
             return in_array($user->email, config('eddy.view_horizon_with_emails'));
         });
     }
